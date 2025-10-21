251364cc6a1016e236b614d3b2d8d243828b2aff
kayleehallett948 <kaylee.hallett@maine.edu>
2025-10-21 17:51:25 -0400

    Update MyBinaryTree.java

--- MyBinaryTree.java
+++ MyBinaryTree.java
@@ -58,4 +58,21 @@ public class MyBinaryTree<E extends Comparable<E>> {
             insert(element);
         }
     }
+
+    public void bfsTree() {
+        if (root = null) {
+            return;
+        Queue<Node<E>> q = new ArrayDeque<Node<E>>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node<E> node = q.remove();
+            System.out.print(node.data + " ");
+            if (node.left != null) {
+                q.add(node.left);
+            }
+            if (node.right != null) {
+                q.add(node.right);
+            }
+            System.out.printlnt();
+        }
 }
